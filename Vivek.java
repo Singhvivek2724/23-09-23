4f13b7ce0022d6e80f3227fefe518ebe70823030
SInghvivek2724 <rajputvivek8578@gmail.com>
2023-09-23 21:43:31 +0530

    23-09-23

---
+++ Vivek.java
@@ -0,0 +1,14 @@
+
+package pack1.java;
+public class Vivek
+{
+	public int cube(int x)
+	{
+		return x*x*x;
+	}
+public int square(int x)
+	{
+		return x*x;
+	}
+}
+		
---
+++ Zoo.java
@@ -0,0 +1,11 @@
+class Zoo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello vivekj");
+		for(String x:args)
+		{
+			System.out.println(x + " ");
+		}
+	}
+}
